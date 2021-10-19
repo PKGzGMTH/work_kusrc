45f0b004cac1c8cb1e017fbcdd1cac00d556549a
PKGzGMTH <peakungg@gmail.com>
2021-10-19 14:09:38 +0700

    add c++ file

---
+++ C++/main.cpp
@@ -0,0 +1,8 @@
+// use command: g++ .\work_kusrc\C++\main.cpp -o .\work_kusrc\C++\main ; d:\.github\work_kusrc\C++\main.exe
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
