f1db1ec3ec611e3e99bddead8ac9f790e6e15081
PKGzGMTH <peakungg@gmail.com>
2021-10-25 22:17:02 +0700

    Test random num

--- esp32s3_project/main/esp32s3_project.c
+++ esp32s3_project/main/esp32s3_project.c
@@ -1,4 +1,5 @@
 // H2install library (.h file)  https://vcpkg.io/en/getting-started.html
+// use 921600 byte speed baud rate
 #include <stdio.h>
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
@@ -8,37 +9,32 @@
 #include "led_strip.h"
 
 #define BLINK_GPIO 48
+#define row 48
+#define col 48
 
 static led_strip_t *pStrip_a;
-uint8_t R,G,B;
-
-static void configure_led(void)
-{
-    /* LED strip initialization with the GPIO and pixels number*/
-    pStrip_a = led_strip_init(0, BLINK_GPIO, 1);
-    /* Set all LED off to clear all pixels */
-    pStrip_a->clear(pStrip_a, 50);
-}
+uint8_t R,G,B,data[row][col];
+int start = 0;
 
 void app_main(void)
 {
     char *ourTaskName = pcTaskGetName(NULL);
+    pStrip_a = led_strip_init(0, BLINK_GPIO, 1);
+    pStrip_a->clear(pStrip_a, 50);
     ESP_LOGI(ourTaskName, "Starting UP...\n");
-    configure_led();
     printf("this is prinf() function!\n\n");
 
     while (1){
-        
-        ESP_LOGI(ourTaskName, "COLOR R:%d G:%d B%d", R = rand()%12, G = rand()%12, B = rand()%12);
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        pStrip_a->set_pixel(pStrip_a, 0, R, G, B);
-        /* Refresh the strip to send data */
+        printf("%c", start);
+        pStrip_a->set_pixel(pStrip_a, 0, 0, 6, 0);
         pStrip_a->refresh(pStrip_a, 100);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-        ESP_LOGI(ourTaskName, "Turn OFF.");
-        /* Set all LED off to clear all pixels */
+        for (int i = 0; i < 4; i++){
+            for (int j = 1; j < 255; j++){
+                //int value = (random()%99)+1;
+                printf("%c",j);
+            }
+        }
         pStrip_a->clear(pStrip_a, 50);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
