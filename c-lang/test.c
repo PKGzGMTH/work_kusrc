e38521f87a7036a88a1c5af2e95f42b7d610870c
PKGzGMTH <peakungg@gmail.com>
2021-10-28 22:49:54 +0700

    add base2 of int4

--- c-lang/test.c
+++ c-lang/test.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
 
-int main(void)
+int base[16][4] = {
+    {0,0,0,0},
+    {0,0,0,1},
+    {0,0,1,0},
+    {0,0,1,1},
+    {0,1,0,0},
+    {0,1,0,1},
+    {0,1,1,0},
+    {0,1,1,1},
+    {1,0,0,0},
+    {1,0,0,1},
+    {1,0,1,0},
+    {1,0,1,1},
+    {1,1,0,0},
+    {1,1,0,1},
+    {1,1,1,0},
+    {1,1,1,1},
+};
+
+void main(void)
 {
-    char x = 'A';
-    int a = 1;
-    printf("%d", x);
-    printf("%c", a);
+    for(int index = 0; index < 16; index++){
+        int num = ((base[index][0]*8) + (base[index][1]*4) + (base[index][2]*2) + (base[index][3]*1));
+        printf("\n%d", num);
+    }
+
 }
