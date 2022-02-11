2f3eaa6eca8100278a67f3a00cafa30af8a3d243
PKGzGMTH <peakungg@gmail.com>
2022-02-11 13:50:47 +0700

    update 11/2/2022

--- c-lang/compro-14.c
+++ c-lang/compro-14.c
@@ -1,7 +1,6 @@
 #include    <stdio.h>
 #include    <stdlib.h>
 
-
 int main(void)
 {
     char    name[30];
@@ -11,13 +10,12 @@ int main(void)
     FILE *data_t;
     data_t = fopen("data.txt", "r");
 
-    for (int i = 0; i < 3; i++)
+    while (!feof(data_t))
     {
         fscanf(data_t, "%d-%f-%s", &count, &price, name);
         printf("Name = %s\tNumber = %d\t Price = %.2f\tTotal = %.2f\n", name, count, price, count * price);
     }
 
     fclose(data_t);
-
     return (0);
 }
